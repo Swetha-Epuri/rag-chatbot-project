59cfcd44286875404e4ad0d9aa4f24bf0e49619b
Swetha-Epuri <swethaepuri03@gmail.com>
2026-07-14 20:12:47 +0530

    Document preprocessing added

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,39 @@
 from loaders.pdf_loader import PDFLoader
-
+from preprocess.cleaner import TextCleaner
+from config import PDF_DIR
 
 
 def main():
 
-    loader = PDFLoader("data/pdfs")
+    loader = PDFLoader(PDF_DIR)
 
     documents = loader.load_documents()
 
-    print(f"Loaded {len(documents)} pages")
+    print(f"\nLoaded {len(documents)} pages\n")
 
-    print()
+    cleaned_documents = []
 
     for doc in documents:
 
-        print(doc.metadata)
+        cleaned_text = TextCleaner.clean_text(
+            doc.page_content
+        )
+
+        doc.page_content = cleaned_text
+
+        cleaned_documents.append(doc)
+
+    cleaned_documents = TextCleaner.remove_empty_documents(cleaned_documents)
+    
+    for doc in cleaned_documents[:2]:
+
+        print("\nSource :", doc.metadata["source"])
+
+        print("Page :", doc.metadata["page"])
 
         print()
 
         print(doc.page_content[:300])
-
-        print()
 
 if __name__=='__main__':
     main()
